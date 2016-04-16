851251f9a0ae41c817f0d69f74e09308260389a1
Noah Klein <noahklein94@gmail.com>
2016-04-16 02:56:32 -0400

    added main funtions to 5 and 6

--- 5.hs
+++ 5.hs
@@ -1,5 +1,8 @@
+main :: IO ()
+main = print findSmallestDivisibleByAll
+
 divisibleByAll :: Int -> Bool
 divisibleByAll n = all divisibleBy [11..19]
     where divisibleBy = (== 0) . (mod n)
 
-findSmallestDivisibleByAll = head (filter divisibleByAll [20,40..])
+findSmallestDivisibleByAll = head $ filter divisibleByAll [20,40..]
--- 6.hs
+++ 6.hs
@@ -1,3 +1,6 @@
+main :: IO ()
+main = print $ answer 100
+
 sumOfSquares n = sum $ map (\x -> x**2) [1..n]
 
 squareOfSum n = (sum [1..n])**2
