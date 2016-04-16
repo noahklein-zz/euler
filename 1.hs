4e67c1727d43dee003a093eb7109aaf0347d8453
Noah Klein <noahklein94@gmail.com>
2016-04-15 23:53:01 -0400

    added main

--- 1.hs
+++ 1.hs
@@ -1,6 +1,9 @@
-multiples :: Int -> [Int] 
+main :: IO ()
+main = print $ answer 1000
+
+multiples :: Int -> [Int]
 multiples upper =
     [i | i <- [1..upper], i `mod` 5 == 0 || i `mod` 3 ==0]
 
 answer :: Int -> Int
-answer n = sum $ multiples (n-1) 
+answer n = sum $ multiples (n-1)
