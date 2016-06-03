04acb3defc6fffce1637d4ab868f479d1e864b20
Noah Klein <noahklein94@gmail.com>
2016-06-03 18:37:51 -0400

    updated 7.hs with Math import

--- 7.hs
+++ 7.hs
@@ -1,9 +1,7 @@
+import Math
+
 main :: IO ()
 main = print $ answer 10001
 
-sieve :: [Int] -> [Int]
-sieve [] = []
-sieve (p:xs) = p:(sieve [i | i <- xs, i `mod` p /= 0])
-
 answer :: Int -> Int
-answer n = last . (take n) $ sieve (2:[3,5..])
+answer n = last . (take n) $ primes
