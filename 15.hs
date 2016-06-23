8fa477fc2beb9333eb95a1e06b78f4a37a40e988
Noah Klein <noahklein94@gmail.com>
2016-06-23 00:13:12 -0400

    15 solved

---
+++ 15.hs
@@ -0,0 +1,20 @@
+main :: IO ()
+main = print $ paths (grid 2) (0, 0)
+
+type Grid = (Int, Int)
+type Pos = (Int, Int)
+
+grid :: Int -> Grid
+grid x = (x, x)
+
+paths :: Grid -> Pos -> Int
+paths grid@(width, height) pos@(x, y)
+  | x == width && y == height = 1
+  | otherwise                 = paths' pos
+  where paths' = sum . (fmap (paths grid)) . (neighbors grid)
+
+neighbors :: Grid -> Pos -> [Pos]
+neighbors (width, height) (x, y) = mappend xNeighbor yNeighbor
+  where
+    xNeighbor = [(x + 1, y) | x < width]
+    yNeighbor = [(x, y + 1) | y < height]
